e01a56eb64d1cdfc6be0ddf786784cc0f6fc9c80
jjaenie <wogus990519@gmail.com>
2025-12-16 15:03:29 +0900

    http 메서드 수정

--- src/main/java/com/logilink/eureka/client/postapplication/controller/PostController.java
+++ src/main/java/com/logilink/eureka/client/postapplication/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.logilink.eureka.client.postapplication.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class PostController {
-	@PostMapping
+	@GetMapping
 	public ResponseEntity<String> post() {
 		return ResponseEntity.ok("Hello world");
 	}
